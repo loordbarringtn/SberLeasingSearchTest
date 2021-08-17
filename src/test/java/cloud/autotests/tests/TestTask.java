69946c852124d2114179c1a5af2ff84c279f91cb
lordbarrington <quantas@gmail.com>
2021-08-18 01:49:15 +0300

    code refactoring again

--- src/test/java/cloud/autotests/tests/TestTask.java
+++ src/test/java/cloud/autotests/tests/TestTask.java
@@ -8,19 +8,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.Keys;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byId;
-import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
-import static java.time.Duration.ofSeconds;
 
 public class TestTask extends TestBase {
     private static final String BASE_URL = "https://www.google.com/ru";
     private static final String TEXT_TO_SEARCH = "СберЛизинг — официальный сайт лизинговой компании";
     CarSelection carSelection = new CarSelection();
-    SelenideElement elementWeCheck = $(byXpath("//h1[@class='h2']"));
-
+    SelenideElement elementWeCheck =$x("//h1[@class='h2']");
 
     @org.junit.jupiter.api.Test
     @Description("Тестовое задание для СберЛизинг")
@@ -59,31 +55,29 @@ public class TestTask extends TestBase {
             carSelection
                     .getFilterCategoryList("Модель");
 
-            $(byXpath("//label[normalize-space()='A4']")).click();
+           $x("//label[normalize-space()='A4']").click();
             $(".horizontal-filter-block__property-title").
                     shouldHave(Condition.text("Стоимость автомобиля")).click();
             Selenide.sleep(2000);
 
-            $(byXpath("//div[@class='range-slider-values__right']//input[@type='number']")).
+           $x("//div[@class='range-slider-values__right']//input[@type='number']").
                     click();
 
             if (isMac()) {
-                $(byXpath("//div[@class='range-slider-values__right']//input[@type='number']")).
+               $x("//div[@class='range-slider-values__right']//input[@type='number']").
                         sendKeys(Keys.COMMAND + "a");
             } else {
-                $(byXpath("//div[@class='range-slider-values__right']//input[@type='number']")).
+               $x("//div[@class='range-slider-values__right']//input[@type='number']").
                         sendKeys(Keys.CONTROL + "a");
             }
 
-            $(byXpath("//div[@class='range-slider-values__right']//input[@type='number']")).
+           $x("//div[@class='range-slider-values__right']//input[@type='number']").
                     sendKeys(Keys.BACK_SPACE);
 
-            $(byXpath("//div[@class='range-slider-values__right']//input[@type='number']")).
+           $x("//div[@class='range-slider-values__right']//input[@type='number']").
                     setValue("3500000");
-
-
-
-            SelenideElement slider2 = $(byXpath("//div[@aria-label='slider between 67 and 612']//div[@class='el-slider__bar']"));
+            
+            SelenideElement slider2 =$x("//div[@aria-label='slider between 67 and 612']//div[@class='el-slider__bar']");
 
             if ($x("//div[@class='col-lg-7 ml-lg-0 ml-md-20 pb-4 pr-lg-90 pt-40 pt-lg-60 px-30 px-lg-30 px-sm-30 px-sm-60 py-30']").
                     isDisplayed()) {
@@ -97,25 +91,25 @@ public class TestTask extends TestBase {
                     isDisplayed()) {
                 $x("//div[@class='modal-present__close']").click();
             }
-            $(byXpath("//label[contains(text(),'передний')]")).click();
-            $(byXpath("//label[contains(text(),'автомат')]")).click();
-            $(byXpath("//label[@title='седан']")).click();
+           $x("//label[contains(text(),'передний')]").click();
+           $x("//label[contains(text(),'автомат')]").click();
+           $x("//label[@title='седан']").click();
 
             //$(byXpath("//button[contains(text(),'Закрыть')]")).click();
-            $(byXpath("//div[contains(text(),'Тип кузова')]")).scrollIntoView(true);
-            $(byXpath("//label[@for='arrFilter_237_3706474592']")).click();
-            $(byXpath("//div[@class='horizontal-filter-block__selector-current-value']//input[@type='text']"))
+           $x("//div[contains(text(),'Тип кузова')]").scrollIntoView(true);
+           $x("//label[@for='arrFilter_237_3706474592']").click();
+           $x("//div[@class='horizontal-filter-block__selector-current-value']//input[@type='text']")
                     .click();
-            $(byXpath("//label[contains(text(),'белый')]")).click();
-            $(byXpath("//div[contains(text(),'Тип топлива')]")).click();
+           $x("//label[contains(text(),'белый')]").click();
+           $x("//div[contains(text(),'Тип топлива')]").click();
         });
 
         step("5)нажать на кнопку Показать все предложения", () -> {
-            $(byXpath("//a[contains(text(),'Показать все предложения')]")).click();
+           $x("//a[contains(text(),'Показать все предложения')]").click();
         });
 
         step("6)выбрать любой из списка", () -> {
-            $(byXpath("(//*[@class=\"sbl-btn sbl-btn_medium w-100\"] )[1]")).click();
+           $x("(//*[@class=\"sbl-btn sbl-btn_medium w-100\"] )[1]").click();
         });
 
         step("7)сделать проверку того, что выбранная марка автомобиля соответствует марке в общем списке из п.6", () -> {
