2a1b78c304d4c5d1dbe2d9279b24dc903fd964c3
lordbarrington <quantas@gmail.com>
2021-08-17 23:58:29 +0300

    major code refactoring

---
+++ src/test/java/cloud/autotests/tests/CarSelection.java
@@ -0,0 +1,23 @@
+package cloud.autotests.tests;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CarSelection {
+
+    protected void getFilterSelection(String filterChoice) {
+        $x("//label[contains(text(),'" + filterChoice + "')]").click();
+    }
+
+    protected void getFilterCategoryList(String categoryChoice) {
+        $(byXpath("//span[contains(text(),'" + categoryChoice + "')]")).click();
+    }
+
+
+
+
+
+
+
+}
